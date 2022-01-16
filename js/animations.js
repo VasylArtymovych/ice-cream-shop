b24e8253e72c3935fc46862e3ca5ef396e2f26d4
VasylArtymovych <art777vasss@gmail.com>
2022-01-16 22:22:01 +0200

    added animations to hero

---
+++ js/animations.js
@@ -0,0 +1,10 @@
+const animItems = document.querySelectorAll("_inime-items");
+
+if (animItems.length > 0) {
+    function animOnScroll(params) {
+        for (let index = 0; index < animItems.length; index++) {
+            const animItems = animItems[index];
+            
+        }
+    }
+}

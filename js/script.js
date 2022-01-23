dcf45a3688b9fb8cb711c441f1bbcc699f9d3a83
VasylArtymovych <art777vasss@gmail.com>
2022-01-23 14:03:00 +0200

    upgreded dealers slider

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 $(document).ready(function () {
-    $('.dealers__list').slick();
+    $('.dealers__list').slick({
+        arrows: true,
+        dots: true,
+        adaptiveHeight: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 300,
+        easing: "liner",
+        infinite: false,
+        initialSlide: 0,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        pauseOnFocus: true,
+        pauseOnHover: true,
+        pauseOnDotsHover: true,
+        draggable: true,
+        swipe: true,
+        waitForAnimate: false,
+    });
 }); 
